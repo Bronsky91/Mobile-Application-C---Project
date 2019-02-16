1f284fdc91b11694f78ab437e8783dc52d5c1a74
Bryan Reed <bryan87reed@gmail.com>
2019-02-16 14:45:24 -0700

    Rough UI outline

---
+++ C971/C971/CoursePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace C971
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CoursePage : ContentPage
+	{
+		public CoursePage ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AssessmentsPage());
+        }
+    }
+}
